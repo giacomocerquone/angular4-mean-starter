59f380a091582909d9513022ece283470731ffa2
Giacomo Cerquone <cerquone@gmail.com>
2017-10-05 16:29:12 +0200

    adding user as a model in order to perform login

---
+++ models/User.js
@@ -0,0 +1,48 @@
+const mongoose = require('mongoose');
+const bcrypt = require('bcrypt-nodejs');
+const jwt = require('jsonwebtoken');
+
+const userSchema = new mongoose.Schema({
+  email: { type: String, unique: true },
+  password: String
+}, { timestamps: true });
+
+/**
+ * Password hash middleware.
+ */
+userSchema.pre('save', function save(next) {
+  const user = this;
+  if (!user.isModified('password')) { return next(); }
+  bcrypt.genSalt(10, (err, salt) => {
+    if (err) { return next(err); }
+    bcrypt.hash(user.password, salt, null, (err, hash) => {
+      if (err) { return next(err); }
+      user.password = hash;
+      next();
+    });
+  });
+});
+
+/**
+ * Helper method for validating user's password.
+ */
+userSchema.methods.comparePassword = function comparePassword(candidatePassword, cb) {
+  bcrypt.compare(candidatePassword, this.password, (err, isMatch) => {
+    cb(err, isMatch);
+  });
+};
+
+userSchema.methods.generateJwt = function() {
+  var expiry = new Date();
+  expiry.setDate(expiry.getDate() + 7);
+
+  return jwt.sign({
+    _id: this._id,
+    email: this.email,
+    exp: parseInt(expiry.getTime() / 1000),
+  }, process.env.JWT_SECRET);
+};
+
+const User = mongoose.model('User', userSchema);
+
+module.exports = User;
